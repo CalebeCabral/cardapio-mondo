fadd6c231f480af04bca3c7b0f9d14e6a227a3c9
Calebe Cabral <calebe.cabral16@outlook.com>
2020-08-14 17:11:46 -0300

    Função Scrollspy

--- js/index.js
+++ js/index.js
@@ -1,21 +1,35 @@
 $(document).ready(function () {
+  init();
+});
+
+// INIT
+function init() {
+  scrollSpy();
   navIndicator.init();
+  navPosition.stickyNav();
+}
+
+// HANDLERS
+$(".nav-categorias ul li").on("click", function (e) {
+  e.preventDefault();
+  scrollAnchor($(this));
 });
 
-$(".nav-categorias ul li").on("click", function () {
-  navIndicator.move($(this));
+$(window).on("scroll", function () {
+  navPosition.stickyNav();
+  scrollSpy();
 });
 
+// Functions
 const navIndicator = {
   indicator: $(".indicador"),
   init() {
-    const elem = $(".nav-categorias ul li").eq(0);
-    this.indicator.css("left", this.setPos(elem))
+    const elem = $(".nav-categorias ul li.active");
+    this.indicator.css("left", this.setPos(elem));
+    console.log("Init")
   },
   move(elem) {
-    navIndicator.indicator.animate({
-      left: this.setPos(elem)
-    }, 300);
+    this.indicator.css("left", this.setPos(elem));
   },
   setPos(elem) {
     const left = Math.round(elem[0].offsetLeft),
@@ -25,4 +39,51 @@ const navIndicator = {
 
     return indicatorPos;
   }
+}
+
+const navPosition = {
+  navOffset: $("nav.nav-categorias")[0].offsetTop,
+  stickyNav() {
+    if (window.pageYOffset > this.navOffset) {
+      $("header").addClass("fixed-nav-categorias");
+    } else {
+      $("header").removeClass("fixed-nav-categorias");
+    }
+  }
+}
+
+function scrollAnchor(self) {
+  const navIndex = self.index(),
+    windowOffset = $("main .categoria").eq(navIndex).find(".cat-anchor").offset().top;
+
+  $("html").animate({
+    scrollTop: windowOffset
+  }, 400);
+}
+
+function scrollSpy() {
+  let categorias = [];
+  let current;
+
+  $("main .categoria").each(function (i) {
+    if ($(this).offset().top <= $(window).scrollTop() + 60) {
+      categorias[i] = this;
+      current = categorias.length - 1;
+    }
+  });
+
+  if (current == undefined) current = 0;
+
+  if (!$(".nav-categorias ul li").eq(current).hasClass("active")) {
+    $(".nav-categorias ul li").removeClass("active")
+    $(".nav-categorias ul li").eq(current).addClass("active");
+    navIndicator.move($(".nav-categorias ul li.active"));
+    navFocus();
+  }
+}
+
+function navFocus() {
+  $(".nav-categorias").animate({
+    scrollLeft: $(".nav-categorias ul li.active").offset().left - 50
+  }, 200);
 }
